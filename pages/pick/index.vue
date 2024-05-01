b5095e79be76c84a78659012fc0d5ef579ee32f9
WKK <toutopia0@gmail.com>
2024-05-01 23:19:28 +0900

    -pick 화면, 카드 선택 애니메이션 이벤트 완료

--- pages/pick/index.vue
+++ pages/pick/index.vue
@@ -1,75 +1,105 @@
 <template>
   <div class="relative">
     <!-- 중앙 카드 영역 -->
-    <div
-      class="relative min-h-screen border flex justify-center items-center z-0"
-    >
+    <div class="relative min-h-screen border flex justify-center items-center">
       <div
-        v-for="(number, index) in defaultArray"
+        v-for="(cardInfo, index) in defaultArray"
         :key="index"
         class="absolute cursor-pointer z-10"
         v-motion
         :initial="vMotionInitialInfo"
         :enter="getVMotionEnterInfo(index)"
-        @click="clickCardDeck"
+        @click="() => clickCardDeck(cardInfo)"
       >
         <PokemonCard>
           {{ index }}
         </PokemonCard>
       </div>
+      <!-- TODO: 왼쪽 카드 영역 -->
+      <div
+        v-for="(cardInfo, index) in selectedCardList.slice(0, 5)"
+        :key="index"
+        class="absolute"
+        v-motion
+        :initial="{
+          opacity: 0,
+          y: 0,
+          x: 0,
+        }"
+        :enter="{
+          opacity: 1,
+          x: cardInfo?.position[0] ?? 0,
+          y: cardInfo?.position[1] ?? 0,
+          transition: {
+            damping: 15,
+            mass: 0.1,
+          },
+        }"
+      >
+        <PokemonCard> {{ cardInfo?.cardNumber }} </PokemonCard>
+      </div>
+      <!-- TODO: 오른쪽 카드 영역 -->
+      <div
+        v-for="(cardInfo, index) in selectedCardList.slice(5, 10)"
+        :key="index"
+        class="absolute"
+        v-motion
+        :initial="{
+          opacity: 0,
+          y: 0,
+          x: 0,
+        }"
+        :enter="{
+          opacity: 1,
+          x: cardInfo?.position[0] ?? 0,
+          y: cardInfo?.position[1] ?? 0,
+          transition: {
+            damping: 15,
+            mass: 0.1,
+          },
+        }"
+      >
+        <PokemonCard> {{ cardInfo?.cardNumber }} </PokemonCard>
+      </div>
     </div>
     <!-- 선택 카드 영역 -->
+    <!-- <div
+      class="absolute start-0 top-0 w-[100%] min-h-screen border flex items-center justify-between"
+    > -->
+    <!-- TODO: 왼쪽 카드 영역 -->
+    <!-- <div
+      class="absolute top-0 min-h-screen w-[400px] ms-10 bg-red-700 z-50"
+    ></div> -->
+    <!-- TODO: 오른쪽 카드 영역 -->
+    <!-- <div class="flex h-[100%] items-center me-10">
+      <div>
+        <div class="flex flex-col">
+          <div class="mb-10">
+            <PokemonCard> hi </PokemonCard>
+          </div>
+          <div>
+            <PokemonCard> hi </PokemonCard>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col ms-10">
+        <div>
+          <PokemonCard> hi </PokemonCard>
+        </div>
+        <div class="my-10">
+          <PokemonCard> hi </PokemonCard>
+        </div>
+        <div>
+          <PokemonCard> hi </PokemonCard>
+        </div>
+      </div>
+    </div> -->
+    <!-- </div> -->
     <div
-      class="absolute start-0 top-0 w-[100%] min-h-screen border flex z-20 items-center justify-between"
+      class="absolute min-h-screen w-[100%] top-0 flex flex-col justify-center items-center pt-[100px]"
     >
-      <!-- 왼쪽 카드 영역 -->
-      <div class="flex h-[100%] items-center ms-10">
-        <div class="flex flex-col me-10">
-          <div>
-            <PokemonCard> hi </PokemonCard>
-          </div>
-          <div class="my-10">
-            <PokemonCard> hi </PokemonCard>
-          </div>
-          <div>
-            <PokemonCard> hi </PokemonCard>
-          </div>
-        </div>
-        <div>
-          <div class="flex flex-col">
-            <div class="mb-10">
-              <PokemonCard> hi </PokemonCard>
-            </div>
-            <div>
-              <PokemonCard> hi </PokemonCard>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- 오른쪽 카드 영역 -->
-      <div class="flex h-[100%] items-center me-10">
-        <div>
-          <div class="flex flex-col">
-            <div class="mb-10">
-              <PokemonCard> hi </PokemonCard>
-            </div>
-            <div>
-              <PokemonCard> hi </PokemonCard>
-            </div>
-          </div>
-        </div>
-        <div class="flex flex-col ms-10">
-          <div>
-            <PokemonCard> hi </PokemonCard>
-          </div>
-          <div class="my-10">
-            <PokemonCard> hi </PokemonCard>
-          </div>
-          <div>
-            <PokemonCard> hi </PokemonCard>
-          </div>
-        </div>
-      </div>
+      <PokemonCard class="opacity-0"></PokemonCard>
+      <UButton @click="() => shuffle()">Shuffle</UButton>
     </div>
   </div>
 </template>
@@ -78,7 +108,59 @@ import { toRef, onMounted } from "vue";
 
 import unitePokemonList from "@/json/unitePokemonList.json";
 
+// FIXME: 단건으로 컨트롤 할 것인가?
+// 왼쪽 카드 ref
+const nineCardRef = toRef();
+const eightCardRef = toRef();
+const sevenCardRef = toRef();
+const sixCardRef = toRef();
+const fiveCardRef = toRef();
+
+const selectedCardList = toRef([]);
+
 const defaultArray = toRef([]);
+const defaultCardList = toRef([
+  {
+    cardNumber: 0,
+    position: [400, 125],
+  },
+  {
+    cardNumber: 1,
+    position: [400, -125],
+  },
+  {
+    cardNumber: 2,
+    position: [650, 250],
+  },
+  {
+    cardNumber: 3,
+    position: [650, 0],
+  },
+  {
+    cardNumber: 4,
+    position: [650, -250],
+  },
+  {
+    cardNumber: 5,
+    position: [-400, 125],
+  },
+  {
+    cardNumber: 6,
+    position: [-400, -125],
+  },
+  {
+    cardNumber: 7,
+    position: [-650, 250],
+  },
+  {
+    cardNumber: 8,
+    position: [-650, 0],
+  },
+  {
+    cardNumber: 9,
+    position: [-650, -250],
+  },
+]);
 
 const vMotionInitialInfo = toRef({
   scale: 1.3,
@@ -105,27 +187,21 @@ const getVMotionEnterInfo = (index) => {
 // TODO: Suffle
 const shuffle = () => {
   defaultArray.value = [];
+  selectedCardList.value = [];
 
   setTimeout(() => {
-    defaultArray.value = [
-      ...Array(10)
-        .fill(0)
-        .map((value, index) => index)
-        .sort(() => Math.random() - 0.5),
-    ];
+    defaultArray.value = defaultCardList.value;
   }, 300);
 };
 
-const clickCardDeck = () => {
+const clickCardDeck = (cardInfo) => {
   defaultArray.value = defaultArray.value.slice(0, -1);
+
+  selectedCardList.value.push(cardInfo);
 };
 
 onMounted(() => {
-  defaultArray.value = [
-    ...Array(10)
-      .fill(0)
-      .map((value, index) => index),
-  ];
+  defaultArray.value = defaultCardList.value;
 });
 </script>
 <style>
