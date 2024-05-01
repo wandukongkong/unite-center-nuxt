111553683808ab9205fb504d56dd73d26fb04234
WKK <toutopia0@gmail.com>
2024-05-02 00:30:08 +0900

    -pick 화면, 버튼 세팅

--- pages/pick/index.vue
+++ pages/pick/index.vue
@@ -62,44 +62,67 @@
         <PokemonCard> {{ cardInfo?.cardNumber }} </PokemonCard>
       </div>
     </div>
-    <!-- 선택 카드 영역 -->
-    <!-- <div
-      class="absolute start-0 top-0 w-[100%] min-h-screen border flex items-center justify-between"
-    > -->
-    <!-- TODO: 왼쪽 카드 영역 -->
-    <!-- <div
-      class="absolute top-0 min-h-screen w-[400px] ms-10 bg-red-700 z-50"
-    ></div> -->
-    <!-- TODO: 오른쪽 카드 영역 -->
-    <!-- <div class="flex h-[100%] items-center me-10">
-      <div>
-        <div class="flex flex-col">
-          <div class="mb-10">
-            <PokemonCard> hi </PokemonCard>
-          </div>
-          <div>
-            <PokemonCard> hi </PokemonCard>
-          </div>
-        </div>
-      </div>
-      <div class="flex flex-col ms-10">
-        <div>
-          <PokemonCard> hi </PokemonCard>
-        </div>
-        <div class="my-10">
-          <PokemonCard> hi </PokemonCard>
-        </div>
-        <div>
-          <PokemonCard> hi </PokemonCard>
-        </div>
-      </div>
-    </div> -->
-    <!-- </div> -->
     <div
       class="absolute min-h-screen w-[100%] top-0 flex flex-col justify-center items-center pt-[100px]"
     >
       <PokemonCard class="opacity-0"></PokemonCard>
-      <UButton @click="() => shuffle()">Shuffle</UButton>
+      <div class="flex items-center">
+        <UButton
+          class="w-[40px] h-[40px] mx-1 py-0 flex justify-center"
+          trailing-icon="i-heroicons-arrow-path-16-solid"
+          @click="() => resetCardDeck()"
+        >
+        </UButton>
+        <UPopover>
+          <UButton
+            class="w-[40px] h-[40px] mx-1 py-0 flex justify-center"
+            color="white"
+            trailing-icon="i-heroicons-cog-6-tooth"
+          />
+          <template #panel="{ close }">
+            <div class="p-4 flex flex-wrap justify-around">
+              <!-- <Placeholder class="h-20 w-48" /> -->
+              <UTooltip text="Default">
+                <UButton
+                  class="bg-[#7e7e7e] hover:bg-[#dbdada] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+              <UTooltip text="Attack">
+                <UButton
+                  class="bg-[#f15438] hover:bg-[#eeb4aa] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+              <UTooltip text="Support">
+                <UButton
+                  class="bg-[#fecc51] hover:bg-[#e8d39d] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+              <UTooltip text="Balance">
+                <UButton
+                  class="bg-[#ce5fd3] hover:bg-[#e1a0e5] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+              <UTooltip text="Speed">
+                <UButton
+                  class="bg-[#29a5e3] hover:bg-[#b4e1f8] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+              <UTooltip text="Defence">
+                <UButton
+                  class="bg-[#aced5b] hover:bg-[#c5dea5] m-1 w-[15px] h-[15px] rounded-xl"
+                  @click="close"
+                ></UButton>
+              </UTooltip>
+            </div>
+          </template>
+        </UPopover>
+        <!-- material-symbols:settings -->
+      </div>
     </div>
   </div>
 </template>
@@ -108,16 +131,7 @@ import { toRef, onMounted } from "vue";
 
 import unitePokemonList from "@/json/unitePokemonList.json";
 
-// FIXME: 단건으로 컨트롤 할 것인가?
-// 왼쪽 카드 ref
-const nineCardRef = toRef();
-const eightCardRef = toRef();
-const sevenCardRef = toRef();
-const sixCardRef = toRef();
-const fiveCardRef = toRef();
-
 const selectedCardList = toRef([]);
-
 const defaultArray = toRef([]);
 const defaultCardList = toRef([
   {
@@ -185,7 +199,7 @@ const getVMotionEnterInfo = (index) => {
 };
 
 // TODO: Suffle
-const shuffle = () => {
+const resetCardDeck = () => {
   defaultArray.value = [];
   selectedCardList.value = [];
 
