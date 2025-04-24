4f81d97dd79ae559f4e8b94290b1c206736513ba
WKK <toutopia0@gmail.com>
2025-04-24 20:31:58 +0900

    토너먼트 임시 오브젝트 푸시

--- components/Bracket.vue
+++ components/Bracket.vue
@@ -1,120 +1,118 @@
 <script setup>
-import { toRef, computed } from "vue";
+import { toRef, computed } from 'vue';
 
 const props = defineProps({
-  teamList: {
-    type: Array,
-    default: () => [
-      {
-        teamName: "TeamA",
-      },
-      {
-        teamName: "TeamB",
-      },
-      {
-        teamName: "TeamC",
-      },
-      {
-        teamName: "TeamD",
-      },
-      {
-        teamName: "TeamE",
-      },
-    ],
-  },
+	teamList: {
+		type: Array,
+		default: () => [
+			{
+				teamName: 'TeamA',
+			},
+			{
+				teamName: 'TeamB',
+			},
+			{
+				teamName: 'TeamC',
+			},
+			{
+				teamName: 'TeamD',
+			},
+			{
+				teamName: 'TeamE',
+			},
+		],
+	},
 });
 
 // TODO: 하위 3팀을 제외하고 1매치만 남는지 체크
-const checkRoundTeam = (list) => {
-  const lastRoundInfo = list.at(-1);
-  const lastTeamList = lastRoundInfo.matchList.reduce((result, matchInfo) => {
-    matchInfo.teamList.forEach((teamInfo) => result.push(teamInfo));
-  }, []);
+const checkRoundTeam = list => {
+	const lastRoundInfo = list.at(-1);
+	const lastTeamList = lastRoundInfo.matchList.reduce((result, matchInfo) => {
+		matchInfo.teamList.forEach(teamInfo => result.push(teamInfo));
+	}, []);
 
-  if (lastRoundInfo.round === 1) {
-    list.push({
-      round: lastRoundInfo.round + 1,
-      matchList: chunkTeam(
-        Math.ceil(Array(lastTeamList.length / 2, 20).fill({}))
-      ),
-    });
-  } else {
-    // check Team 3팀제외하고 1매치 남는지 체크
-    if (lastTeamList.length - 3 === 2) {
-      list.push({
-        round: lastRoundInfo.round + 1,
-        matchList: chunkTeam([
-          {
-            teamName: "",
-          },
-          {
-            teamName: "",
-          },
-        ]),
-      });
+	if (lastRoundInfo.round === 1) {
+		list.push({
+			round: lastRoundInfo.round + 1,
+			matchList: chunkTeam(Math.ceil(Array(lastTeamList.length / 2, 20).fill({}))),
+		});
+	} else {
+		// check Team 3팀제외하고 1매치 남는지 체크
+		if (lastTeamList.length - 3 === 2) {
+			list.push({
+				round: lastRoundInfo.round + 1,
+				matchList: chunkTeam([
+					{
+						teamName: '',
+					},
+					{
+						teamName: '',
+					},
+				]),
+			});
 
-      list.push({
-        round: lastRoundInfo.round + 2,
-        matchList: chunkTeam([
-          {
-            teamName: "",
-          },
-          {
-            teamName: "",
-          },
-          {
-            teamName: "",
-          },
-          {
-            teamName: "",
-          },
-        ]),
-      });
-    } else {
-      list.push({
-        round: lastRoundInfo.round + 1,
-        matchList: chunkTeam(lastTeamList),
-      });
-    }
+			list.push({
+				round: lastRoundInfo.round + 2,
+				matchList: chunkTeam([
+					{
+						teamName: '',
+					},
+					{
+						teamName: '',
+					},
+					{
+						teamName: '',
+					},
+					{
+						teamName: '',
+					},
+				]),
+			});
+		} else {
+			list.push({
+				round: lastRoundInfo.round + 1,
+				matchList: chunkTeam(lastTeamList),
+			});
+		}
 
-    // if (lastTeamList.length > 4) {
-    //   list = checkRoundTeam(list);
-    // } else {
-    // }
-  }
-  return list;
+		// if (lastTeamList.length > 4) {
+		//   list = checkRoundTeam(list);
+		// } else {
+		// }
+	}
+	return list;
 };
 
 const chunkTeam = (list, chunkNumber = 2) => {
-  return list.reduce((result, info, index) => {
-    if (index === 0) {
-      result.push({
-        match: 1,
-        config: {
-          x: 0,
-          y: 20,
-        },
-        teamList: [],
-      });
-    }
+	return list.reduce((result, info, index) => {
+		if (index === 0) {
+			result.push({
+				match: 1,
+				config: {
+					x: 0,
+					y: 20,
+				},
+				teamList: [],
+			});
+		}
 
-    if (result.at(-1).teamList.length === chunkNumber) {
-      const lastMatchInfo = result.at(-1);
+		if (result.at(-1).teamList.length === chunkNumber) {
+			const lastMatchInfo = result.at(-1);
 
-      result.push({
-        match: lastMatchInfo.match + 1,
-        config: {
-          x: 0,
-          y: lastMatchInfo.config.y + 100,
-        },
-        teamList: [],
-      });
-    }
+			result.push({
+				match: lastMatchInfo.match + 1,
+				config: {
+					x: 0,
+					y: lastMatchInfo.config.y + 100,
+				},
+				teamList: [],
+			});
+		}
 
-    result.at(-1).teamList.push(info);
+		result.at(-1).teamList.push(info);
 
-    return result;
-  }, []);
+		return result;
+	}, []);
 };
 
 // matchList: [
@@ -124,107 +122,226 @@ const chunkTeam = (list, chunkNumber = 2) => {
 //   }
 // ]
 
+// TODO:
 const convertedTournamentList = computed(() => {
-  let firstRoundList = [
-    {
-      round: 1,
-      matchList: chunkTeam(props.teamList),
-    },
-  ];
+	// let firstRoundList = [
+	//   {
+	//     round: 1,
+	//     matchList: chunkTeam(props.teamList),
+	//   },
+	// ];
 
-  const otherRoundList = checkRoundTeam(firstRoundList);
+	// const otherRoundList = checkRoundTeam(firstRoundList);
 
-  return firstRoundList;
+	return [
+		{
+			round: 1,
+			matchList: [
+				{
+					match: 1,
+					config: {
+						stroke: 'black',
+						strokeWidth: 1,
+						y: 30,
+					},
+					teamList: [
+						{
+							teamName: 'teamA',
+						},
+						{
+							teamName: 'teamB',
+						},
+					],
+				},
+				{
+					match: 2,
+					config: {
+						stroke: 'black',
+						strokeWidth: 1,
+						y: 150,
+					},
+					teamList: [
+						{
+							teamName: 'teamC',
+						},
+						{
+							teamName: 'teamD',
+						},
+					],
+				},
+				{
+					match: 3,
+					config: {
+						stroke: 'black',
+
+						strokeWidth: 1,
+						y: 270,
+					},
+					teamList: [
+						{
+							teamName: 'teamE',
+						},
+						{
+							teamName: 'teamF',
+						},
+					],
+				},
+			],
+		},
+		{
+			round: 2,
+			matchList: [
+				{
+					match: 4,
+					config: {
+						stroke: 'black',
+						strokeWidth: 1,
+						y: 90,
+					},
+					teamList: [
+						{
+							teamName: '',
+						},
+						{
+							teamName: '',
+						},
+					],
+				},
+				{
+					match: 5,
+					config: {
+						stroke: 'black',
+						strokeWidth: 1,
+						y: 210,
+					},
+					teamList: [
+						{
+							teamName: '',
+						},
+					],
+				},
+			],
+		},
+		{
+			round: 3,
+			matchList: [
+				{
+					match: 4,
+					config: {
+						stroke: 'black',
+						strokeWidth: 1,
+						y: 150,
+					},
+					teamList: [
+						{
+							teamName: '',
+						},
+						{
+							teamName: '',
+						},
+					],
+				},
+			],
+		},
+	];
 });
 
 const configKonva = toRef({
-  width: window.screen.width,
-  height: window.screen.height,
+	width: window.screen.width,
+	height: window.screen.height,
 });
 
 const matchRectInfo = toRef({
-  // draggable: true,
+	x: -5,
+	width: 180,
+	height: 90,
+	stroke: 'black',
+	strokeWidth: 1,
 });
 
 const teamRectInfo = toRef({
-  x: 0,
-  width: 170,
-  height: 30,
-  stroke: "black",
-  strokeWidth: 1,
-  fill: "white",
-  cornerRadius: 3,
-  shadowBlur: 0,
-  // draggable: true,
+	x: 0,
+	width: 170,
+	height: 30,
+	stroke: 'black',
+	strokeWidth: 1,
+	fill: 'white',
+	cornerRadius: 3,
+	shadowBlur: 0,
+	// draggable: true,
 });
 
 const teamNameTextConfig = toRef({
-  x: 5,
-  y: 5,
-  fontSize: 10,
+	x: 5,
+	y: 5,
+	fontSize: 10,
 });
 
-const hoverMouse = (e) => {
-  e.target.to({
-    scaleX: 1.03,
-    scaleY: 1.03,
-    duration: 0.05,
-    shadowBlur: 5,
-  });
+const hoverMouse = e => {
+	e.target.to({
+		scaleX: 1.03,
+		scaleY: 1.03,
+		duration: 0.05,
+		shadowBlur: 5,
+	});
 };
 
-const leaveMouse = (e) => {
-  e.target.to({
-    scaleX: 1,
-    scaleY: 1,
-    duration: 0.1,
-    shadowBlur: 0,
-  });
+const leaveMouse = e => {
+	e.target.to({
+		scaleX: 1,
+		scaleY: 1,
+		duration: 0.1,
+		shadowBlur: 0,
+	});
 };
 </script>
 <template>
-  <v-stage :config="configKonva">
-    <!-- Round -->
-    <v-layer
-      v-for="(tournamentInfo, tournamentInfoIndex) in convertedTournamentList"
-      :key="tournamentInfoIndex"
-      :config="{
-        x: 110 + 300 * tournamentInfoIndex,
-        y: 50,
-      }"
-    >
-      <v-text
-        :config="{ text: `${tournamentInfo.round} Round`, fontSize: 15 }"
-      />
-      <!-- match -->
-      <v-group
-        v-for="(matchInfo, matchInfoIndex) in tournamentInfo.matchList"
-        :key="matchInfoIndex"
-        :config="{
-          x: matchInfo?.config?.x ?? 0,
-          y: matchInfo?.config?.y ?? 0,
-        }"
-      >
-        <!-- team -->
-        <v-group
-          v-for="(teamInfo, teamInfoIndex) in matchInfo.teamList"
-          :config="{
-            y: 10 + 40 * teamInfoIndex,
-          }"
-        >
-          <v-rect
-            :key="teamInfoIndex"
-            :config="{
-              ...teamRectInfo,
-            }"
-          >
-          </v-rect>
-          <v-text
-            :config="{ text: teamInfo.teamName, ...teamNameTextConfig }"
-          ></v-text>
-        </v-group>
-      </v-group>
-    </v-layer>
-  </v-stage>
+	<v-stage :config="configKonva">
+		<!-- Round -->
+		<v-layer
+			v-for="(tournamentInfo, tournamentInfoIndex) in convertedTournamentList"
+			:key="tournamentInfoIndex"
+			:config="{
+				x: 110 + 300 * tournamentInfoIndex,
+				y: 50,
+			}"
+		>
+			<v-text :config="{ text: `${tournamentInfo.round} Round`, fontSize: 15 }" />
+			<!-- match -->
+			<v-group
+				v-for="(matchInfo, matchInfoIndex) in tournamentInfo.matchList"
+				:key="matchInfoIndex"
+				:config="{
+					x: matchInfo?.config?.x ?? 0,
+					y: matchInfo?.config?.y ?? 0,
+					stroke: 'black',
+					strokeWidth: 1,
+				}"
+			>
+				<v-rect
+					:config="{
+						...matchRectInfo,
+					}"
+				>
+				</v-rect>
+				<!-- team -->
+				<v-group
+					v-for="(teamInfo, teamInfoIndex) in matchInfo.teamList"
+					:config="{
+						y: 10 + 40 * teamInfoIndex,
+					}"
+				>
+					<v-rect
+						:key="teamInfoIndex"
+						:config="{
+							...teamRectInfo,
+						}"
+					>
+					</v-rect>
+					<v-text :config="{ text: teamInfo.teamName, ...teamNameTextConfig }"></v-text>
+				</v-group>
+			</v-group>
+		</v-layer>
+	</v-stage>
 </template>
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,272 +1,246 @@
 <script setup>
-import { toRef, computed, onMounted, onBeforeMount } from "vue";
-import { useMotionProperties, useMotionControls } from "@vueuse/motion";
+import { toRef, computed, onMounted, onBeforeMount } from 'vue';
+import { useMotionProperties, useMotionControls } from '@vueuse/motion';
 
 const { isMobile } = useDevice();
 const router = useRouter();
 
 // container motion
 const container = toRef();
-const { motionProperties: containerMotionProperties } =
-  useMotionProperties(container);
-const { apply: applyContainerMotion } = useMotionControls(
-  containerMotionProperties,
-  {
-    initial: {
-      scale: 1,
-      opacity: 0,
-    },
-    enter: {
-      y: 0,
-      opacity: 1,
-    },
-    leave: {
-      y: 15,
-      // bounce: 4,
-      transition: {
-        duration: 150,
-        ease: "easyOut",
-        repeatDelay: 0,
-        // repeatType: "reverse",
-      },
-    },
-    leave2: {
-      y: -200,
-      opacity: 0,
-      bounce: 0,
-      transition: {
-        type: "spring",
-        stiffness: 250,
-        damping: 25,
-        mass: 0.5,
-      },
-    },
-  }
-);
+const { motionProperties: containerMotionProperties } = useMotionProperties(container);
+const { apply: applyContainerMotion } = useMotionControls(containerMotionProperties, {
+	initial: {
+		scale: 1,
+		opacity: 0,
+	},
+	enter: {
+		y: 0,
+		opacity: 1,
+	},
+	leave: {
+		y: 15,
+		// bounce: 4,
+		transition: {
+			duration: 150,
+			ease: 'easyOut',
+			repeatDelay: 0,
+			// repeatType: "reverse",
+		},
+	},
+	leave2: {
+		y: -200,
+		opacity: 0,
+		bounce: 0,
+		transition: {
+			type: 'spring',
+			stiffness: 250,
+			damping: 25,
+			mass: 0.5,
+		},
+	},
+});
 
 // Logo Motion
 const logoRef = toRef();
 const { motionProperties: logoMotionProperties } = useMotionProperties(logoRef);
 const { apply: applyLogoMotion } = useMotionControls(logoMotionProperties, {
-  initial: {
-    scale: 0.8,
-    opacity: 0,
-  },
-  enter: {
-    y: 0,
-    opacity: 1,
-  },
-  hover: {
-    y: -5,
-    transition: {
-      duration: 400,
-      ease: "easyOut",
-      repeat: Infinity,
-      repeatDelay: 0,
-      repeatType: "reverse",
-    },
-  },
-  leave: {
-    opacity: 0,
-  },
-  stop: {
-    y: 0,
-  },
+	initial: {
+		scale: 0.8,
+		opacity: 0,
+	},
+	enter: {
+		y: 0,
+		opacity: 1,
+	},
+	hover: {
+		y: -5,
+		transition: {
+			duration: 400,
+			ease: 'easyOut',
+			repeat: Infinity,
+			repeatDelay: 0,
+			repeatType: 'reverse',
+		},
+	},
+	leave: {
+		opacity: 0,
+	},
+	stop: {
+		y: 0,
+	},
 });
 
 // object3 Motion
 const object3Ref = toRef();
-const { motionProperties: object3MotionProperties } =
-  useMotionProperties(object3Ref);
-const { apply: applyObject3Motion } = useMotionControls(
-  object3MotionProperties,
-  {
-    initial: {
-      x: 87,
-      y: -420,
-      scale: 0.8,
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-    },
-    hover: {
-      y: -425,
-      transition: {
-        duration: 400,
-        ease: "easyOut",
-        repeat: Infinity,
-        repeatDelay: 0,
-        repeatType: "reverse",
-      },
-    },
-    leave: {
-      opacity: 0,
-    },
-    stop: {
-      y: -420,
-    },
-  }
-);
+const { motionProperties: object3MotionProperties } = useMotionProperties(object3Ref);
+const { apply: applyObject3Motion } = useMotionControls(object3MotionProperties, {
+	initial: {
+		x: 87,
+		y: -420,
+		scale: 0.8,
+		opacity: 0,
+	},
+	enter: {
+		opacity: 1,
+	},
+	hover: {
+		y: -425,
+		transition: {
+			duration: 400,
+			ease: 'easyOut',
+			repeat: Infinity,
+			repeatDelay: 0,
+			repeatType: 'reverse',
+		},
+	},
+	leave: {
+		opacity: 0,
+	},
+	stop: {
+		y: -420,
+	},
+});
 
 // object4 motion
 const object4Ref = toRef();
-const { motionProperties: object4MotionProperties } =
-  useMotionProperties(object4Ref);
-const { apply: applyObject4Motion } = useMotionControls(
-  object4MotionProperties,
-  {
-    initial: {
-      x: 550,
-      y: -160,
-      scale: 0.8,
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-    },
-    hover: {
-      y: -165,
-      transition: {
-        duration: 400,
-        ease: "easyOut",
-        repeat: Infinity,
-        repeatDelay: 0,
-        repeatType: "reverse",
-      },
-    },
-    leave: {
-      opacity: 0,
-    },
-    stop: {
-      y: -160,
-    },
-  }
-);
+const { motionProperties: object4MotionProperties } = useMotionProperties(object4Ref);
+const { apply: applyObject4Motion } = useMotionControls(object4MotionProperties, {
+	initial: {
+		x: 550,
+		y: -160,
+		scale: 0.8,
+		opacity: 0,
+	},
+	enter: {
+		opacity: 1,
+	},
+	hover: {
+		y: -165,
+		transition: {
+			duration: 400,
+			ease: 'easyOut',
+			repeat: Infinity,
+			repeatDelay: 0,
+			repeatType: 'reverse',
+		},
+	},
+	leave: {
+		opacity: 0,
+	},
+	stop: {
+		y: -160,
+	},
+});
 
 //object 6 motion
 const object6Ref = toRef();
-const { motionProperties: object6MotionProperties } =
-  useMotionProperties(object6Ref);
-const { apply: applyObject6Motion } = useMotionControls(
-  object6MotionProperties,
-  {
-    initial: {
-      x: 370,
-      y: -435,
-      scale: 0.7,
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-    },
-    hover: {
-      y: -440,
-      transition: {
-        duration: 400,
-        ease: "easyOut",
-        repeat: Infinity,
-        repeatDelay: 0,
-        repeatType: "reverse",
-      },
-    },
-    leave: {
-      opacity: 0,
-    },
-    stop: {
-      y: -435,
-    },
-  }
-);
+const { motionProperties: object6MotionProperties } = useMotionProperties(object6Ref);
+const { apply: applyObject6Motion } = useMotionControls(object6MotionProperties, {
+	initial: {
+		x: 370,
+		y: -435,
+		scale: 0.7,
+		opacity: 0,
+	},
+	enter: {
+		opacity: 1,
+	},
+	hover: {
+		y: -440,
+		transition: {
+			duration: 400,
+			ease: 'easyOut',
+			repeat: Infinity,
+			repeatDelay: 0,
+			repeatType: 'reverse',
+		},
+	},
+	leave: {
+		opacity: 0,
+	},
+	stop: {
+		y: -435,
+	},
+});
 
 // object7 motion
 const object7Ref = toRef();
-const { motionProperties: object7MotionProperties } =
-  useMotionProperties(object7Ref);
-const { apply: applyObject7Motion } = useMotionControls(
-  object7MotionProperties,
-  {
-    initial: {
-      x: -20,
-      y: -200,
-      scale: 0.7,
-      opacity: 0,
-    },
-    enter: {
-      opacity: 1,
-    },
-    hover: {
-      y: -205,
-      transition: {
-        duration: 400,
-        ease: "easyOut",
-        repeat: Infinity,
-        repeatDelay: 0,
-        repeatType: "reverse",
-      },
-    },
-    leave: {
-      opacity: 0,
-    },
-    stop: {
-      y: -200,
-    },
-  }
-);
+const { motionProperties: object7MotionProperties } = useMotionProperties(object7Ref);
+const { apply: applyObject7Motion } = useMotionControls(object7MotionProperties, {
+	initial: {
+		x: -20,
+		y: -200,
+		scale: 0.7,
+		opacity: 0,
+	},
+	enter: {
+		opacity: 1,
+	},
+	hover: {
+		y: -205,
+		transition: {
+			duration: 400,
+			ease: 'easyOut',
+			repeat: Infinity,
+			repeatDelay: 0,
+			repeatType: 'reverse',
+		},
+	},
+	leave: {
+		opacity: 0,
+	},
+	stop: {
+		y: -200,
+	},
+});
 
-const clickMenu = async (pageName) => {
-  applyLogoMotion("leave");
-  await applyContainerMotion("leave");
-  await applyContainerMotion("leave2");
+const clickMenu = async pageName => {
+	applyLogoMotion('leave');
+	await applyContainerMotion('leave');
+	await applyContainerMotion('leave2');
 
-  router.push(pageName);
+	router.push(pageName);
 };
 
 onBeforeMount(() => {
-  applyLogoMotion("initial");
-  applyObject3Motion("initial");
-  applyObject4Motion("initial");
-  applyObject6Motion("initial");
-  applyObject7Motion("initial");
-  applyContainerMotion("initial");
+	applyLogoMotion('initial');
+	applyObject3Motion('initial');
+	applyObject4Motion('initial');
+	applyObject6Motion('initial');
+	applyObject7Motion('initial');
+	applyContainerMotion('initial');
 });
 
 onMounted(() => {
-  applyLogoMotion("enter");
-  applyObject3Motion("enter");
-  applyObject4Motion("enter");
-  applyObject6Motion("enter");
-  applyObject7Motion("enter");
-  applyContainerMotion("enter");
+	applyLogoMotion('enter');
+	applyObject3Motion('enter');
+	applyObject4Motion('enter');
+	applyObject6Motion('enter');
+	applyObject7Motion('enter');
+	applyContainerMotion('enter');
 });
 </script>
 <template>
-  <div class="flex flex-col min-h-screen justify-center items-center">
-    <div class="flex justify-end ms-5 items-center w-[940px]">
-      <img
-        ref="logoRef"
-        class="opacity-0 h-[30px] py-1"
-        src="@/public/img/pokemon/uniteCenterLogo.png"
-      />
-    </div>
-    <div ref="container" class="flex rounded-xl shadow-2xl w-[940px] opacity-0">
-      <div class="flex flex-col ps-7 pe-5 py-6">
-        <div
-          class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200"
-        >
-          <div class="flex justify-center w-[50px]">
-            <img src="@/public/img/pokemon/ball.png" class="h-4" />
-          </div>
-          <UButton
-            class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
-            color="transparent"
-            @mouseover="() => applyLogoMotion('hover')"
-            @mouseleave="() => applyLogoMotion('stop')"
-          >
-            <strong>Home</strong>
-          </UButton>
-        </div>
-        <!-- <div
+	<div class="flex flex-col min-h-screen justify-center items-center">
+		<div class="flex justify-end ms-5 items-center w-[940px]">
+			<img ref="logoRef" class="opacity-0 h-[30px] py-1" src="@/public/img/pokemon/uniteCenterLogo.png" />
+		</div>
+		<div ref="container" class="flex rounded-xl shadow-2xl w-[940px] opacity-0">
+			<div class="flex flex-col ps-7 pe-5 py-6">
+				<div class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200">
+					<div class="flex justify-center w-[50px]">
+						<img src="@/public/img/pokemon/ball.png" class="h-4" />
+					</div>
+					<UButton
+						class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
+						color="transparent"
+						@mouseover="() => applyLogoMotion('hover')"
+						@mouseleave="() => applyLogoMotion('stop')"
+					>
+						<strong>Home</strong>
+					</UButton>
+				</div>
+				<!-- <div
           class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200"
         >
           <div class="flex justify-center w-[50px]">
@@ -281,39 +255,35 @@ onMounted(() => {
             <strong>Dashboard</strong>
           </UButton>
         </div> -->
-        <div
-          class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200"
-        >
-          <div class="flex justify-center w-[50px]">
-            <img src="@/public/img/pokemon/object3.png" class="h-4" />
-          </div>
-          <UButton
-            class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
-            color="transparent"
-            @click="() => clickMenu('update')"
-            @mouseover="() => applyObject3Motion('hover')"
-            @mouseleave="() => applyObject3Motion('stop')"
-          >
-            <strong>Update</strong>
-          </UButton>
-        </div>
-        <div
-          class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200"
-        >
-          <div class="flex justify-center w-[50px]">
-            <img src="@/public/img/pokemon/object7.png" class="h-4" />
-          </div>
-          <UButton
-            class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
-            color="transparent"
-            @click="() => clickMenu('pick')"
-            @mouseover="() => applyObject7Motion('hover')"
-            @mouseleave="() => applyObject7Motion('stop')"
-          >
-            <strong>Random Pick</strong>
-          </UButton>
-        </div>
-        <!-- <div
+				<div class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200">
+					<div class="flex justify-center w-[50px]">
+						<img src="@/public/img/pokemon/object3.png" class="h-4" />
+					</div>
+					<UButton
+						class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
+						color="transparent"
+						@click="() => clickMenu('update')"
+						@mouseover="() => applyObject3Motion('hover')"
+						@mouseleave="() => applyObject3Motion('stop')"
+					>
+						<strong>Update</strong>
+					</UButton>
+				</div>
+				<div class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200">
+					<div class="flex justify-center w-[50px]">
+						<img src="@/public/img/pokemon/object7.png" class="h-4" />
+					</div>
+					<UButton
+						class="bg-white w-[120px] text-black shadow-none py-0 hover:bg-white"
+						color="transparent"
+						@click="() => clickMenu('pick')"
+						@mouseover="() => applyObject7Motion('hover')"
+						@mouseleave="() => applyObject7Motion('stop')"
+					>
+						<strong>Random Pick</strong>
+					</UButton>
+				</div>
+				<!-- <div
           class="flex items-center mb-5 hover:scale-[1.04] ease-in-out duration-200"
         >
           <div class="flex justify-center w-[50px]">
@@ -326,7 +296,7 @@ onMounted(() => {
             <strong>Circle</strong>
           </UButton>
         </div> -->
-        <!-- <div
+				<!-- <div
           class="flex items-center hover:scale-[1.04] ease-in-out duration-200"
         >
           <div class="flex justify-center w-[50px]">
@@ -341,43 +311,43 @@ onMounted(() => {
             <strong>Tournament</strong>
           </UButton>
         </div> -->
-      </div>
-      <div class="relative border-8 border-white rounded-xl">
-        <img class="rounded-md" src="@/public/img/pokemon/pokemoncenter.png" />
-        <img
-          class="absolute opacity-0 cursor-pointer"
-          ref="object3Ref"
-          src="@/public/img/pokemon/object3.png"
-          @click="() => router.push('/update')"
-          @mouseover="() => applyObject3Motion('hover')"
-          @mouseleave="() => applyObject3Motion('stop')"
-        />
-        <img
-          class="absolute opacity-0 cursor-pointer"
-          ref="object4Ref"
-          src="@/public/img/pokemon/object4.png"
-          @click="() => router.push('/tournament')"
-          @mouseover="() => applyObject4Motion('hover')"
-          @mouseleave="() => applyObject4Motion('stop')"
-        />
-        <img
-          class="absolute opacity-0 cursor-pointer"
-          ref="object6Ref"
-          src="@/public/img/pokemon/object6.png"
-          @click="() => router.push('/dashboard')"
-          @mouseover="() => applyObject6Motion('hover')"
-          @mouseleave="() => applyObject6Motion('stop')"
-        />
-        <img
-          class="absolute opacity-0 cursor-pointer"
-          ref="object7Ref"
-          src="@/public/img/pokemon/object7.png"
-          @click="() => router.push('/pick')"
-          @mouseover="() => applyObject7Motion('hover')"
-          @mouseleave="() => applyObject7Motion('stop')"
-        />
-      </div>
-    </div>
-  </div>
+			</div>
+			<div class="relative border-8 border-white rounded-xl">
+				<img class="rounded-md" src="@/public/img/pokemon/pokemoncenter.png" />
+				<img
+					class="absolute opacity-0 cursor-pointer"
+					ref="object3Ref"
+					src="@/public/img/pokemon/object3.png"
+					@click="() => router.push('/update')"
+					@mouseover="() => applyObject3Motion('hover')"
+					@mouseleave="() => applyObject3Motion('stop')"
+				/>
+				<img
+					class="absolute opacity-0 cursor-pointer"
+					ref="object4Ref"
+					src="@/public/img/pokemon/object4.png"
+					@click="() => router.push('/tournament')"
+					@mouseover="() => applyObject4Motion('hover')"
+					@mouseleave="() => applyObject4Motion('stop')"
+				/>
+				<img
+					class="absolute opacity-0 cursor-pointer"
+					ref="object6Ref"
+					src="@/public/img/pokemon/object6.png"
+					@click="() => router.push('/dashboard')"
+					@mouseover="() => applyObject6Motion('hover')"
+					@mouseleave="() => applyObject6Motion('stop')"
+				/>
+				<img
+					class="absolute opacity-0 cursor-pointer"
+					ref="object7Ref"
+					src="@/public/img/pokemon/object7.png"
+					@click="() => router.push('/pick')"
+					@mouseover="() => applyObject7Motion('hover')"
+					@mouseleave="() => applyObject7Motion('stop')"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 <style></style>
